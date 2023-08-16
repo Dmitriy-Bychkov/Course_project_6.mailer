5dbfbc1a4155f18c27ef2096e2a855198aeba9e8
Dmitriy Bychkov <582620@gmail.com>
2023-08-17 00:36:15 +0800

    Add Client model for clients

--- mail/models.py
+++ mail/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Client(models.Model):
+    """Модель для клиента сервиса рассылок"""
+
+    client_name = models.CharField(max_length=30, verbose_name='имя')
+    client_email = models.EmailField(max_length=100, verbose_name='email')
+
+    def __str__(self):
+        return f'{self.client_name}, {self.client_email}'
+
+    class Meta:
+        """Представление написания заголовков в админке"""
+
+        verbose_name = "клиент"
+        verbose_name_plural = "клиенты"
