253f79c19b6c6752ca4a3fa1415bd0359eea6b6d
Dmitriy Bychkov <582620@gmail.com>
2023-08-17 00:36:43 +0800

    Add url for the Client model

--- mail/urls.py
+++ mail/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
 from mail.apps import MailConfig
+from mail.views import ClientListView
 
 app_name = MailConfig.name
 
 urlpatterns = [
-
+    path('', ClientListView.as_view(), name='list')
 ]
