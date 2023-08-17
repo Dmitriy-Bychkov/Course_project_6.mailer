bbf0f1bd45d0090eff47f0b1ae1f3decfa0b628c
Dmitriy Bychkov <582620@gmail.com>
2023-08-17 14:15:58 +0800

    Add message model

--- mail/models.py
+++ mail/models.py
@@ -1,17 +1,36 @@
 from django.db import models
 
+NULLABLE = {'null': True, 'blank': True}
+
 
 class Client(models.Model):
     """Модель для клиента сервиса рассылок"""
 
     client_name = models.CharField(max_length=30, verbose_name='имя')
     client_email = models.EmailField(max_length=100, verbose_name='email')
+    comment = models.TextField(verbose_name='Комментарий', **NULLABLE)
 
     def __str__(self):
         return f'{self.client_name}, {self.client_email}'
 
     class Meta:
-        """Представление написания заголовков в админке"""
+        """Представление написания заголовков для клиента в админке"""
 
         verbose_name = "клиент"
         verbose_name_plural = "клиенты"
+
+
+class Message(models.Model):
+    """Модель рассылочного сообщения"""
+
+    message_subject = models.CharField(max_length=180, verbose_name='тема письма')
+    message_text = models.TextField(verbose_name='cообщение')
+
+    def __str__(self):
+        return f'{self.message_subject}, {self.message_text}'
+
+    class Meta:
+        """Представление написания заголовков для письма в админке"""
+
+        verbose_name = "сообщение"
+        verbose_name_plural = "сообщения"
