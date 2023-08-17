73685856fee5237e48ca6ab78e09271beabfa3f4
Dmitriy Bychkov <582620@gmail.com>
2023-08-17 14:18:05 +0800

    Change urls for the app

--- mail/urls.py
+++ mail/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
 
 from mail.apps import MailConfig
-from mail.views import ClientListView
+from mail.views import ClientListView, MessageCreateView, MessageListView, MessageDetailView, MessageUpdateView, \
+    MessageDelete, IndexView
 
 app_name = MailConfig.name
 
 urlpatterns = [
-    path('', ClientListView.as_view(), name='list')
+    path('', IndexView.as_view(), name='index'),
+    path('434/', ClientListView.as_view(), name='client_list'),
+    path('create/', MessageCreateView.as_view(), name='message_create'),
+    path('messages/', MessageListView.as_view(), name='message_list'),
+    path('view/<int:pk>/', MessageDetailView.as_view(), name='message_view'),
+    path('edit/<int:pk>/', MessageUpdateView.as_view(), name='message_edit'),
+    path('delete/<int:pk>/', MessageDelete.as_view(), name='message_delete'),
 ]
