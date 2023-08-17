ec3eea3f772f3ea4e921f289cca819f3ad0b8ffd
Dmitriy Bychkov <582620@gmail.com>
2023-08-17 14:14:49 +0800

    Register message in the admin panel

--- mail/admin.py
+++ mail/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from mail.models import Client
+from mail.models import Client, Message
 
 
 @admin.register(Client)
@@ -8,4 +8,11 @@ class ClientAdmin(admin.ModelAdmin):
     """Представление раздела - "Клиенты" в админке"""
 
     list_display = ('id', 'client_name', 'client_email')
-    search_fields = ('client_name', 'client_email',)
+    search_fields = ('client_name', 'client_email', 'comment',)
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    """Представление раздела - "Сообщения" в админке"""
+
+    list_display = ('id', 'message_subject', 'message_text')
+    search_fields = ('message_subject', 'message_text',)
