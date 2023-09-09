b7da15b9871b1deba224e8dbd4d9f0dad6a81de5
Dmitriy Bychkov <582620@gmail.com>
2023-09-09 18:21:03 +0800

    .

--- mail/models.py
+++ mail/models.py
@@ -8,7 +8,7 @@ class Client(models.Model):
 
     client_name = models.CharField(max_length=30, verbose_name='имя')
     client_email = models.EmailField(max_length=100, verbose_name='email')
-    comment = models.TextField(verbose_name='Комментарий', **NULLABLE)
+    comment = models.TextField(verbose_name='комментарий', **NULLABLE)
 
     def __str__(self):
         return f'{self.client_name}, {self.client_email}'
@@ -34,3 +34,67 @@ class Message(models.Model):
 
         verbose_name = "сообщение"
         verbose_name_plural = "сообщения"
+
+
+class Mailing(models.Model):
+    """Модель рассылки"""
+
+    FREQUENCY_CHOICE = (
+        ("day", "день"),
+        ("week", "неделя"),
+        ("month", "месяц"),
+    )
+
+    STATUS_CHOICE = (
+        ("created", "Создана"),
+        ("completed", "Завершена"),
+        ("started", "Запущена"),
+    )
+
+    start_time = models.DateTimeField(verbose_name='старт рассылки')
+    completion_time = models.DateTimeField(verbose_name='завершение рассылки')
+    frequency = models.CharField(max_length=30, choices=FREQUENCY_CHOICE, default='day',
+                                 verbose_name='периодичность')
+    status = models.CharField(max_length=20, choices=STATUS_CHOICE, default='created', verbose_name='статус')
+    message = models.ForeignKey(Message, verbose_name='сообщение', on_delete=models.CASCADE)
+    clients = models.ManyToManyField(Client, verbose_name='клиенты')
+    is_active = models.BooleanField(default=True, verbose_name='статус активности')
+
+    # def get_status(self):
+    #     now = timezone.now()
+    #     if self.commence_time < now < self.completion_time:
+    #         self.status = "commenced"
+    #     elif now > self.completion_time:
+    #         self.status = "completed"
+    #     self.save()
+    #     return self.status
+
+    def __str__(self):
+        return f'{self.message}: {self.frequency}'
+
+    class Meta:
+        """Представление написания рассылки в админке"""
+
+        verbose_name = 'рассылка'
+        verbose_name_plural = 'рассылки'
+
+
+class Log(models.Model):
+    """Модель логов рассылки"""
+
+    STATUS_CHOICE = (
+        ("success", "Успешно"),
+        ("failed", "Неуспешно"),
+    )
+    mailing = models.ForeignKey(Mailing, verbose_name='рассылка', on_delete=models.CASCADE)
+    last_attempt = models.DateTimeField(verbose_name='время последней попытки')
+    status = models.CharField(max_length=7, choices=STATUS_CHOICE, verbose_name='статус попытки')
+
+    def __str__(self):
+        return f'{self.mailing} ({self.last_attempt}) - {self.status}'
+
+    class Meta:
+        """Представление написания логов в админке"""
+
+        verbose_name = 'лог'
+        verbose_name_plural = 'логи'
