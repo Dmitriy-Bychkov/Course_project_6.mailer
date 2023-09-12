1854fa086d7f7e1e122f0a194febdc59cca266ca
Dmitriy Bychkov <582620@gmail.com>
2023-09-12 10:45:43 +0800

    Change 'get_context_data' for tasks logic

--- mail/views.py
+++ mail/views.py
@@ -1,8 +1,10 @@
+import random
 from django.urls import reverse_lazy, reverse
 from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView, TemplateView
 
+from blog.models import Blog
 from mail.forms import MailingForm, ClientForm, MessageForm
-from mail.models import Client, Message, Mailing
+from mail.models import Client, Message, Mailing, Log
 
 
 class IndexView(TemplateView):
@@ -13,6 +15,30 @@ class IndexView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Главная страница'
+
+        # Получение количества рассылок всего
+        total_mailings = Mailing.objects.count()
+        context['total_mailings'] = total_mailings
+
+        # Получение количества активных рассылок
+        active_mailings = Mailing.objects.filter(is_active=True).count()
+        context['active_mailings'] = active_mailings
+
+        # Получение количества уникальных клиентов для рассылок
+        unique_clients = Client.objects.count()
+        context['unique_clients'] = unique_clients
+
+        # Получение всех статей блога
+        all_blogs = list(Blog.objects.all())
+
+        if len(all_blogs) >= 3:
+            # Получение трёх случайных статей из блога
+            random_blogs = random.sample(all_blogs, 3)
+        else:
+            # Если количество статей меньше 3, выводим все статьи
+            random_blogs = all_blogs
+
+        context['random_blogs'] = random_blogs
 
         return context
 
@@ -120,3 +146,9 @@ class MailingDelete(DeleteView):
 
     model = Mailing
     success_url = reverse_lazy('mail:mailing_list')
+
+
+class LogListView(ListView):
+    """Контроллер для просмотра списка логов рассылок"""
+
+    model = Log
