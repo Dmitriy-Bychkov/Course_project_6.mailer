7181e3d9111e7e3aad3483dccbfaf55f8ce66f84
Dmitriy Bychkov <582620@gmail.com>
2023-09-13 11:54:24 +0800

    Add new users app and realize CRUD system for it + email registration and confirmation

---
+++ users/forms.py
@@ -0,0 +1,36 @@
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordResetForm, SetPasswordForm
+from users.models import User
+from mail.forms import StyleFormMixin
+from django import forms
+
+
+class UserRegisterForm(StyleFormMixin, UserCreationForm):
+    class Meta:
+        model = User
+        fields = ["email", "phone", "country", "avatar", "password1", "password2"]
+
+
+class UserProfileForm(StyleFormMixin, UserChangeForm):
+    class Meta:
+        model = User
+        fields = ["email", "password", "first_name", "last_name", "phone", "country", "avatar"]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['password'].widget = forms.HiddenInput()
+
+
+class PasswordForm(StyleFormMixin, forms.Form):
+    new_password = forms.CharField(label='Новый пароль', widget=forms.PasswordInput)
+    confirm_password = forms.CharField(label='Подтвердите пароль', widget=forms.PasswordInput)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        new_password = cleaned_data.get('new_password')
+        confirm_password = cleaned_data.get('confirm_password')
+
+        if new_password and confirm_password and new_password != confirm_password:
+            raise forms.ValidationError('Пароли не совпадают')
+
+        return cleaned_data
