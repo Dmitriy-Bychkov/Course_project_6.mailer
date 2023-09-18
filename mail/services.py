9da400014d79436594712fb712e7661557fb7117
Dmitriy Bychkov <582620@gmail.com>
2023-09-18 17:12:19 +0800

    Add new function for start sending mailings

--- mail/services.py
+++ mail/services.py
@@ -1,8 +1,10 @@
 from django.conf import settings
 from django.core.mail import send_mail
 
+from mail.models import Mailing
 
-def send_email(subject, message, recipient_list):
+
+def send_mail_task(subject, message, recipient_list):
     '''
     Отправляет e-mail пользователю через втроенную функцию send_mail
     :param subject: заголовок сообщения
@@ -13,6 +15,18 @@ def send_email(subject, message, recipient_list):
     from_email = settings.EMAIL_HOST_USER
 
     try:
-        send_mail(subject, message, from_email, recipient_list, fail_silently=False)
-    except:
-        print('Ошибка отправки')
+        return send_mail(subject, message, from_email, recipient_list, fail_silently=False)
+    except Exception as e:
+        print(f'Ошибка отправки {e}')
+        raise
+
+
+def send_mailing_task():
+    """ Вызывает отправку рассылок """
+
+    # выбрать все подходящие рассылки
+    mailing_list = Mailing.get_objects_for_send()
+    # запустить цикл по этим рассылкам
+    for mailing in mailing_list:
+        # вызвать метод send
+        mailing.send()
