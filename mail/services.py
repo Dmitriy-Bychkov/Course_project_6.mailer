b36ceb2c952de2d4dcc7c84386b90176d6e0f245
Dmitriy Bychkov <582620@gmail.com>
2023-09-19 15:45:36 +0800

    Remove send_mail_task function from the services file to avoid circular imports and add get_blog_cache function to cache blog list page

--- mail/services.py
+++ mail/services.py
@@ -1,24 +1,7 @@
-from django.conf import settings
-from django.core.mail import send_mail
-
+from blog.models import Blog
 from mail.models import Mailing
-
-
-def send_mail_task(subject, message, recipient_list):
-    '''
-    Отправляет e-mail пользователю через втроенную функцию send_mail
-    :param subject: заголовок сообщения
-    :param message: текст сообщения
-    :param recipient_list: список e-mail адресов получателей
-    '''
-
-    from_email = settings.EMAIL_HOST_USER
-
-    try:
-        return send_mail(subject, message, from_email, recipient_list, fail_silently=False)
-    except Exception as e:
-        print(f'Ошибка отправки {e}')
-        raise
+from django.conf import settings
+from django.core.cache import cache
 
 
 def send_mailing_task():
@@ -30,3 +13,17 @@ def send_mailing_task():
     for mailing in mailing_list:
         # вызвать метод send
         mailing.send()
+
+
+def get_blog_cache():
+    """ Настройка низкоуровневого кеширования для списка статей блога """
+
+    if settings.CACHE_ENABLED:
+        cache_key = 'blog_list'
+        blog_list = cache.get(cache_key)
+        if blog_list is None:
+            category_list = Blog.objects.all()
+            cache.set(cache_key, category_list, 60)
+    else:
+        blog_list = Blog.objects.all()
+    return blog_list
