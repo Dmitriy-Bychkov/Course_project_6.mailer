9b76e703427bfbc35cad312b80d3e12a23bcbe33
Dmitriy Bychkov <582620@gmail.com>
2023-09-21 02:18:07 +0800

    Change the description of the command

--- users/management/commands/start_send_mail.py
+++ users/management/commands/start_send_mail.py
@@ -3,7 +3,7 @@ from mail.services import send_mailing_task
 
 
 class Command(BaseCommand):
-    """Класс для отправки сообщений Кроном"""
+    """Класс для отправки сообщений через командную строку"""
 
     def handle(self, *args, **options):
         send_mailing_task()
